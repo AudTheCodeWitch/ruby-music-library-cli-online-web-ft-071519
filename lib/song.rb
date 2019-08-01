a4708968a70292fce5380bf63db6b903e5cb5542
Audrea Cook <audreacwork@gmail.com>
2019-08-01 22:53:07 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+    self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    Song.new(name)
+    self
+  end
+  
+  
+end
